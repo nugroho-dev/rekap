3c315766112e2dcb92196ae8ab92e1233c7974e5
nugroho-dev <didik.ngr@gmail.com>
2024-08-14 15:17:53 +0700

    perubahan query

--- app/Http/Controllers/Api/ApiCekSicantikController.php
+++ app/Http/Controllers/Api/ApiCekSicantikController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Resources\CekSicantikResource;
 use App\Models\Proses;
 use Illuminate\Support\Facades\DB;
+use Termwind\Components\Raw;
 
 class ApiCekSicantikController extends Controller
 {
@@ -20,7 +21,7 @@ class ApiCekSicantikController extends Controller
         //get all posts
         $no_permohonan= request('no_permohonan');
         $email= request('email');
-        $posts = Proses::where('no_permohonan','=', $no_permohonan)->where('email','=', $email)->select('jenis_izin','nama','nama_proses','no_permohonan','email')->orderBy('id_proses_permohonan', 'asc')->get();
+        $posts = Proses::where('no_permohonan','=', $no_permohonan)->where('email','=', $email)->select(DB::raw('COALESCE(end_date,start_date, "-") as date'),'jenis_izin','nama','nama_proses','no_permohonan','email','status')->orderBy('id_proses_permohonan', 'asc')->get();
     
         //return collection of posts as a resource
         return new CekSicantikResource(true, 'List Data Posts', $posts);
