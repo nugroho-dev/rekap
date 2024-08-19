d913cc456ec910cf5ca49f1fbc2d43eb3a86d2f1
nugroho-dev <didik.ngr@gmail.com>
2024-08-19 12:04:37 +0700

    api tte

--- app/Http/Controllers/Api/TteController.php
+++ app/Http/Controllers/Api/TteController.php
@@ -21,7 +21,7 @@ class TteController extends Controller
         //get all posts
         $no_permohonan= request('no_permohonan');
         $email= request('email');
-        $posts = Proses::where('no_permohonan','=', $no_permohonan)->where('no_hp','=', $email)->whereNotIn('status',['Drop'])->where('jenis_proses_id','=', 40)->select(DB::raw('COALESCE(CONCAT("https://sicantik.go.id/webroot/files/signed/",file_signed_report), "zero") as tte'),'jenis_izin','nama','nama_proses','no_permohonan','email','status')->get();
+        $posts = Proses::where('no_permohonan','=', $no_permohonan)->where('no_hp','=', $email)->whereNotIn('status',['Drop'])->where('jenis_proses_id','=', 40)->select(DB::raw('COALESCE(CONCAT("https://sicantik.go.id/webroot/files/signed/",file_signed_report)) as tte'),'jenis_izin','nama','nama_proses','no_permohonan','email','status')->get();
     
         //return collection of posts as a resource
         return new CekSicantikResource(true, 'List Data Posts', $posts);
