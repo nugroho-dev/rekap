9e6a1be0f3b91ec17a0c1459b2ee9e8b7199d5c3
nugroho-dev <didik.ngr@gmail.com>
2025-02-06 15:29:47 +0700

    upadte updaet or create

--- app/Console/Commands/SicantikSych.php
+++ app/Console/Commands/SicantikSych.php
@@ -44,10 +44,8 @@ class SicantikSych extends Command
         $chunks = array_chunk($items, 100);
 
         foreach ($chunks as $chunk) {
-            $dataToInsert = [];
-
             foreach ($chunk as $val) {
-            $dataToInsert[] = array_intersect_key($val, array_flip([
+            $data = array_intersect_key($val, array_flip([
                 'id_proses_permohonan','alamat', 'data_status', 'default_active', 'del', 'dibuat_oleh', 'diproses_oleh', 'diubah_oleh', 
                 'email', 'end_date', 'file_signed_report', 'instansi_id', 'jenis_izin', 'jenis_izin_id', 
                 'jenis_kelamin', 'jenis_permohonan', 'jenis_proses_id', 'lokasi_izin', 'nama', 'nama_proses', 
@@ -55,16 +53,12 @@ class SicantikSych extends Command
                 'start_date', 'status', 'tgl_dibuat', 'tgl_diubah', 'tgl_lahir', 'tgl_penetapan', 'tgl_pengajuan', 
                 'tgl_pengajuan_time', 'tgl_rekomendasi', 'tgl_selesai', 'tgl_selesai_time', 'tgl_signed_report'
             ]));
-            }
 
-            Proses::upsert($dataToInsert, ['id_proses_permohonan'], [
-            'alamat', 'data_status', 'default_active', 'del', 'dibuat_oleh', 'diproses_oleh', 'diubah_oleh', 
-            'email', 'end_date', 'file_signed_report', 'instansi_id', 'jenis_izin', 'jenis_izin_id', 
-            'jenis_kelamin', 'jenis_permohonan', 'jenis_proses_id', 'lokasi_izin', 'nama', 'nama_proses', 
-            'no_hp', 'no_izin', 'no_permohonan', 'no_rekomendasi', 'no_tlp', 'permohonan_izin_id', 
-            'start_date', 'status', 'tgl_dibuat', 'tgl_diubah', 'tgl_lahir', 'tgl_penetapan', 'tgl_pengajuan', 
-            'tgl_pengajuan_time', 'tgl_rekomendasi', 'tgl_selesai', 'tgl_selesai_time', 'tgl_signed_report'
-            ]);
+            Proses::updateOrCreate(
+                ['id_proses_permohonan' => $data['id_proses_permohonan']],
+                $data
+            );
+            }
         }
         
     }
