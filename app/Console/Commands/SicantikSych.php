0b9700f40d5f5707132d6955de8246328716ffcf
nugroho-dev <didik.ngr@gmail.com>
2025-02-06 07:44:58 +0700

    update json api sych

--- app/Console/Commands/SicantikSych.php
+++ app/Console/Commands/SicantikSych.php
@@ -30,34 +30,35 @@ class SicantikSych extends Command
      */
     public function handle()
     {
-        //$date1 = date("Y-m-01");
-        $date2 = date("Y-m-d");
-        $date1 = Carbon::parse($date2)->subMonth()->format('Y-m-d');
-        $urls = [
-            'https://sicantik.go.id/api/TemplateData/keluaran/42611.json',
-        ];
+        $date2 = Carbon::now()->format('Y-m-d');
+        $date1 = Carbon::now()->subMonth()->format('Y-m-d');
+        $url = 'https://sicantik.go.id/api/TemplateData/keluaran/42611.json';
 
-        foreach (array_chunk($urls, 1) as $chunk) {
-            foreach ($chunk as $url) {
-            $response = Http::retry(10, 1000)->get($url . '?date1=' . $date1 . '&date2=' . $date2);
-            $data = $response->json();
-            $items = $data['data']['data'];
-            foreach (array_chunk($items, 100) as $itemChunk) {
-                foreach ($itemChunk as $val) {
-                Proses::upsert(
-                    array_intersect_key($val, array_flip([
-                    'alamat', 'data_status', 'default_active', 'del', 'dibuat_oleh', 'diproses_oleh', 'diubah_oleh', 
-			'email', 'end_date', 'file_signed_report', 'instansi_id', 'jenis_izin', 'jenis_izin_id', 
-			'jenis_kelamin', 'jenis_permohonan', 'jenis_proses_id', 'lokasi_izin', 'nama', 'nama_proses', 
-			'no_hp', 'no_izin', 'no_permohonan', 'no_rekomendasi', 'no_tlp', 'permohonan_izin_id', 
-			'start_date', 'status', 'tgl_dibuat', 'tgl_diubah', 'tgl_lahir', 'tgl_penetapan', 'tgl_pengajuan', 
-			'tgl_pengajuan_time', 'tgl_rekomendasi', 'tgl_selesai', 'tgl_selesai_time', 'tgl_signed_report'
-                    ])),
-                    ['id_proses_permohonan']
-                );
-                }
+        $response = Http::retry(10, 1000)->get($url, [
+            'date1' => $date1,
+            'date2' => $date2
+        ]);
+
+        $data = $response->json();
+        $items = $data['data']['data'];
+        $chunks = array_chunk($items, 100);
+
+        foreach ($chunks as $chunk) {
+            $dataToInsert = [];
+
+            foreach ($chunk as $val) {
+            $dataToInsert[] = array_intersect_key($val, array_flip([
+                'alamat', 'data_status', 'default_active', 'del', 'dibuat_oleh', 'diproses_oleh', 'diubah_oleh', 
+                'email', 'end_date', 'file_signed_report', 'instansi_id', 'jenis_izin', 'jenis_izin_id', 
+                'jenis_kelamin', 'jenis_permohonan', 'jenis_proses_id', 'lokasi_izin', 'nama', 'nama_proses', 
+                'no_hp', 'no_izin', 'no_permohonan', 'no_rekomendasi', 'no_tlp', 'permohonan_izin_id', 
+                'start_date', 'status', 'tgl_dibuat', 'tgl_diubah', 'tgl_lahir', 'tgl_penetapan', 'tgl_pengajuan', 
+                'tgl_pengajuan_time', 'tgl_rekomendasi', 'tgl_selesai', 'tgl_selesai_time', 'tgl_signed_report'
+            ]));
             }
-            }
+
+            Proses::upsert($dataToInsert, ['id_proses_permohonan']);
         }
+        
     }
 }
