15845e3a09eaa5d5080bdc990e1faf31d3f22200
nugroho-dev <didik.ngr@gmail.com>
2025-03-04 10:44:39 +0700

    update dowload izin sicantik

--- resources/views/admin/nonberusaha/sicantik/index.blade.php
+++ resources/views/admin/nonberusaha/sicantik/index.blade.php
@@ -155,6 +155,9 @@
                                 <a class="dropdown-item" href="{{ url('/sicantik/'.$item->slug.'')}}" >
                                   Lihat Proses
                                 </a>
+                                <a class="dropdown-item" href="https://sicantik.go.id/webroot/files/signed/{{$item->file_signed_report}}" >
+                                  Download Izin
+                                </a>
                               </div>
                             </span>
                            
