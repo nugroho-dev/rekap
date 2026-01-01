e721536dee6e61f4ca0e5568ff6105d3b37de686
nugroho-dev <didik.ngr@gmail.com>
2026-01-01 16:22:48 +0700

    upaddate tabel publik

--- resources/views/publicviews/home/index.blade.php
+++ resources/views/publicviews/home/index.blade.php
@@ -4,26 +4,26 @@
   <div class="col-12">
     <div class="card">
       <div class="table-responsive">
-        <table class="table card-table table-vcenter text-nowrap datatable text-center">
+        <table class="table card-table table-striped table-hover table-vcenter text-nowrap datatable">
           @foreach($kategori as $kat)
             <thead>
               <tr>
-                <th colspan="6" class="bg-gradient-primary text-black text-center fs-4 py-3">{{ $kat->nama }}</th>
+                <th colspan="6" class=" text-black  fs-4 py-3">{{ $kat->nama }}</th>
               </tr>
               <tr class="bg-light">
                 <th class="text-uppercase">Jenis Informasi</th>
-                <th class="text-uppercase">Status Update</th>
-                <th class="text-uppercase">Tanggal Update</th>
-                <th class="text-uppercase">Jumlah Data</th>
-                <th class="text-uppercase">Data Set</th>
-                <th class="text-uppercase">Statistik</th>
+                <th class="text-uppercase text-center">Status Update</th>
+                <th class="text-uppercase text-center">Tanggal Update</th>
+                <th class="text-uppercase text-center">Jumlah Data</th>
+                <th class="text-uppercase text-center">Data Set</th>
+                <th class="text-uppercase text-center">Statistik</th>
               </tr>
             </thead>
             <tbody>
               @forelse($kat->jenisInformasi as $jenis)
                 <tr>
                   <td><strong>{{ $jenis->label }}</strong></td>
-                  <td>
+                  <td class="text-center">
                     @if($jenis->updated_model_at)
                       @php
                         $updated = \Carbon\Carbon::parse($jenis->updated_model_at);
@@ -44,16 +44,16 @@
                       <span class="badge bg-gradient-secondary">Belum diupdate</span>
                     @endif
                   </td>
-                  <td><span class="text-muted">{{ $jenis->updated_model_at ? \Carbon\Carbon::parse($jenis->updated_model_at)->format('d M Y H:i') : '-' }}</span></td>
-                  <td><span class="fw-bold text-primary">{{ number_format($jenis->jumlah ?? 0) }} Data</span></td>
-                  <td>
+                  <td class="text-center"><span class="text-muted">{{ $jenis->updated_model_at ? \Carbon\Carbon::parse($jenis->updated_model_at)->format('d M Y H:i') : '-' }}</span></td>
+                  <td class="text-center"><span class="fw-bold text-primary">{{ number_format($jenis->jumlah ?? 0) }} Data</span></td>
+                  <td class="text-center">
                     @if($jenis->dataset)
                       <a href="{{ $jenis->dataset }}" class="btn btn-outline-success btn-sm" target="_blank"><i class="bi bi-check-circle"></i> Tersedia</a>
                     @else
                       <span class="btn btn-outline-secondary btn-sm disabled"><i class="bi bi-x-circle"></i> Tidak Ada</span>
                     @endif
                   </td>
-                  <td class="text-end">
+                  <td class="text-center">
                     @if($jenis->link_api)
                       <a href="{{ $jenis->link_api }}" class="btn btn-outline-info btn-sm" target="_blank"><i class="bi bi-bar-chart"></i> Tersedia</a>
                     @else
