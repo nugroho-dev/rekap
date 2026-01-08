d5946ac8395c656ef43e16ea40e7ad6ecc8b1983
nugroho-dev <didik.ngr@gmail.com>
2026-01-08 09:04:11 +0700

    update link

--- resources/views/admin/pelayananpm/komitmen/statistik.blade.php
+++ resources/views/admin/pelayananpm/komitmen/statistik.blade.php
@@ -25,7 +25,7 @@
             </div>
             <div class="col-auto ms-auto d-print-none">
                 <div class="btn-list">
-                    <a href="/commitment" class="btn btn-primary d-none d-sm-inline-block">
+                    <a href="{{ url('/commitment') }}" class="btn btn-primary d-none d-sm-inline-block">
                         <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 11l-4 4l4 4m-4 -4h11a4 4 0 0 0 0 -8h-1" /></svg>
                         Kembali
                     </a>
